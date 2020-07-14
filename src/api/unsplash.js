4e85cc4e4b77f06c6d4cf7e015c058e278c74cd1
Ankit patel <36583951+4nkitpatel@users.noreply.github.com>
2020-07-14 19:52:26 +0530

    removed comment from unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// this create a axios client with all the functionality we did this becx of it help us in later apps we can use function instead totally fine np
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
